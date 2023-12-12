4772447d77a77021e5644cdd388487a97204dfb6
Bruno Staine <brunostaine@hotmail.com>
2023-12-12 19:46:30 -0300

    Criando o big card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, MenuTitleComponent, BigCardComponent, SmallCardComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
